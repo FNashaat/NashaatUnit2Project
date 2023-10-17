8242ff7341c96ad9ed70d2826c2387a39bf38618
BT_1N3_29 <farheenn@nycstudents.net>
2023-10-17 10:47:45 -0400

    leaving logic class for now, moving on to linear equation class

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
 public class LinearEquationLogic {
-    private Point point;
+    private LinearEquation linear;
     private Scanner myScanner;
 
     public LinearEquationLogic(){
-        point = null;
+        linear = null;
         myScanner = new Scanner(System.in);
     }
 
-    private getDataPoints(){
+    private void getDataPoints(){
+        System.out.println("Welcome to the linear equation calculator!");
+        System.out.println("Enter coordinate 1: ");
+        int coord1 = myScanner.nextInt();
+        System.out.println("Enter coordinate 2: ");
+        int coord2 = myScanner.nextInt();
+        System.out.println();
+        System.out.println("The two points are: " + coord1 + " and " + coord2);
 
+        int x1 = Integer.parseInt();
+
+    }
+
+    public void start(){
+        getDataPoints();
     }
 }
